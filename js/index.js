ec0b20411d4c4031cffa46961a3e3ff233b3b1bd
John Casey McDonough <johncaseymcd@gmail.com>
2022-12-07 22:14:08 -0800

    Added .prettierrc, finished basic page. Ready for initial deployment. Now playing: Ol by Plaid

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-$(function(){
-  $("#click-for-long").click(function(){
+$(function () {
+  $("#click-for-long").click(function () {
     $("#long-version").removeClass("invisible");
     $("#tldr").addClass("invisible");
     $("#horizontal-spacer").addClass("invisible");
   });
 
-  $("#click-for-tldr").click(function(){
+  $("#click-for-tldr").click(function () {
     $("#tldr").removeClass("invisible");
     $("#long-version").addClass("invisible");
     $("#horizontal-spacer").addClass("invisible");
